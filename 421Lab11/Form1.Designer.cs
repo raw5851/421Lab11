e5527d39823c90cedee45b060231eb30df914f90
raw5851 <raw5851@psu.edu>
2024-04-12 11:59:31 -0400

    Add project files.

---
+++ 421Lab11/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace _421Lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            back = new Button();
+            clearEntry = new Button();
+            clear = new Button();
+            signChange = new Button();
+            squareRoot = new Button();
+            seven = new Button();
+            eight = new Button();
+            nine = new Button();
+            divide = new Button();
+            four = new Button();
+            five = new Button();
+            six = new Button();
+            multiply = new Button();
+            recipricol = new Button();
+            one = new Button();
+            two = new Button();
+            three = new Button();
+            minus = new Button();
+            zer = new Button();
+            dec = new Button();
+            plus = new Button();
+            equals = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // back
+            // 
+            back.BackColor = SystemColors.GradientActiveCaption;
+            back.Location = new Point(12, 68);
+            back.Name = "back";
+            back.Size = new Size(29, 23);
+            back.TabIndex = 0;
+            back.Text = "<-";
+            back.UseVisualStyleBackColor = false;
+            back.Click += button1_Click;
+            // 
+            // clearEntry
+            // 
+            clearEntry.BackColor = SystemColors.GradientActiveCaption;
+            clearEntry.Location = new Point(47, 68);
+            clearEntry.Name = "clearEntry";
+            clearEntry.Size = new Size(32, 23);
+            clearEntry.TabIndex = 1;
+            clearEntry.Text = "CE";
+            clearEntry.UseVisualStyleBackColor = false;
+            clearEntry.Click += button1_Click_1;
+            // 
+            // clear
+            // 
+            clear.BackColor = SystemColors.GradientActiveCaption;
+            clear.Location = new Point(85, 68);
+            clear.Name = "clear";
+            clear.Size = new Size(35, 23);
+            clear.TabIndex = 2;
+            clear.Text = "C";
+            clear.UseVisualStyleBackColor = false;
+            clear.Click += clear_Click;
+            // 
+            // signChange
+            // 
+            signChange.BackColor = SystemColors.GradientActiveCaption;
+            signChange.Location = new Point(126, 68);
+            signChange.Name = "signChange";
+            signChange.Size = new Size(34, 23);
+            signChange.TabIndex = 3;
+            signChange.Text = "+-";
+            signChange.UseVisualStyleBackColor = false;
+            signChange.Click += signChange_Click;
+            // 
+            // squareRoot
+            // 
+            squareRoot.BackColor = SystemColors.GradientActiveCaption;
+            squareRoot.Location = new Point(166, 68);
+            squareRoot.Name = "squareRoot";
+            squareRoot.Size = new Size(54, 23);
+            squareRoot.TabIndex = 4;
+            squareRoot.Text = "^(1/2)";
+            squareRoot.UseVisualStyleBackColor = false;
+            squareRoot.Click += button1_Click_2;
+            // 
+            // seven
+            // 
+            seven.BackColor = SystemColors.GradientActiveCaption;
+            seven.Location = new Point(12, 97);
+            seven.Name = "seven";
+            seven.Size = new Size(29, 23);
+            seven.TabIndex = 5;
+            seven.Text = "7";
+            seven.UseVisualStyleBackColor = false;
+            seven.Click += seven_Click;
+            // 
+            // eight
+            // 
+            eight.BackColor = SystemColors.GradientActiveCaption;
+            eight.Location = new Point(47, 97);
+            eight.Name = "eight";
+            eight.Size = new Size(32, 23);
+            eight.TabIndex = 6;
+            eight.Text = "8";
+            eight.UseVisualStyleBackColor = false;
+            eight.Click += eight_Click;
+            // 
+            // nine
+            // 
+            nine.BackColor = SystemColors.GradientActiveCaption;
+            nine.Location = new Point(85, 97);
+            nine.Name = "nine";
+            nine.Size = new Size(35, 23);
+            nine.TabIndex = 7;
+            nine.Text = "9";
+            nine.UseVisualStyleBackColor = false;
+            nine.Click += nine_Click;
+            // 
+            // divide
+            // 
+            divide.BackColor = SystemColors.GradientActiveCaption;
+            divide.Location = new Point(126, 97);
+            divide.Name = "divide";
+            divide.Size = new Size(34, 23);
+            divide.TabIndex = 8;
+            divide.Text = "/";
+            divide.UseVisualStyleBackColor = false;
+            divide.Click += divide_Click;
+            // 
+            // four
+            // 
+            four.BackColor = SystemColors.GradientActiveCaption;
+            four.Location = new Point(12, 126);
+            four.Name = "four";
+            four.Size = new Size(29, 23);
+            four.TabIndex = 9;
+            four.Text = "4";
+            four.UseVisualStyleBackColor = false;
+            four.Click += four_Click;
+            // 
+            // five
+            // 
+            five.BackColor = SystemColors.GradientActiveCaption;
+            five.Location = new Point(47, 126);
+            five.Name = "five";
+            five.Size = new Size(32, 23);
+            five.TabIndex = 10;
+            five.Text = "5";
+            five.UseVisualStyleBackColor = false;
+            five.Click += five_Click;
+            // 
+            // six
+            // 
+            six.BackColor = SystemColors.GradientActiveCaption;
+            six.Location = new Point(85, 126);
+            six.Name = "six";
+            six.Size = new Size(35, 23);
+            six.TabIndex = 11;
+            six.Text = "6";
+            six.UseVisualStyleBackColor = false;
+            six.Click += six_Click;
+            // 
+            // multiply
+            // 
+            multiply.BackColor = SystemColors.GradientActiveCaption;
+            multiply.Location = new Point(126, 126);
+            multiply.Name = "multiply";
+            multiply.Size = new Size(34, 23);
+            multiply.TabIndex = 12;
+            multiply.Text = "*";
+            multiply.UseVisualStyleBackColor = false;
+            multiply.Click += multiply_Click;
+            // 
+            // recipricol
+            // 
+            recipricol.BackColor = SystemColors.GradientActiveCaption;
+            recipricol.Location = new Point(166, 97);
+            recipricol.Name = "recipricol";
+            recipricol.Size = new Size(54, 52);
+            recipricol.TabIndex = 13;
+            recipricol.Text = "1/x";
+            recipricol.UseVisualStyleBackColor = false;
+            recipricol.Click += recipricol_Click;
+            // 
+            // one
+            // 
+            one.BackColor = SystemColors.GradientActiveCaption;
+            one.Location = new Point(12, 155);
+            one.Name = "one";
+            one.Size = new Size(29, 23);
+            one.TabIndex = 15;
+            one.Text = "1";
+            one.UseVisualStyleBackColor = false;
+            one.Click += one_Click;
+            // 
+            // two
+            // 
+            two.BackColor = SystemColors.GradientActiveCaption;
+            two.Location = new Point(47, 155);
+            two.Name = "two";
+            two.Size = new Size(32, 23);
+            two.TabIndex = 16;
+            two.Text = "2";
+            two.UseVisualStyleBackColor = false;
+            two.Click += two_Click;
+            // 
+            // three
+            // 
+            three.BackColor = SystemColors.GradientActiveCaption;
+            three.Location = new Point(85, 155);
+            three.Name = "three";
+            three.Size = new Size(35, 23);
+            three.TabIndex = 17;
+            three.Text = "3";
+            three.UseVisualStyleBackColor = false;
+            three.Click += three_Click;
+            // 
+            // minus
+            // 
+            minus.BackColor = SystemColors.GradientActiveCaption;
+            minus.Location = new Point(126, 155);
+            minus.Name = "minus";
+            minus.Size = new Size(34, 23);
+            minus.TabIndex = 18;
+            minus.Text = "-";
+            minus.UseVisualStyleBackColor = false;
+            minus.Click += minus_Click;
+            // 
+            // zer
+            // 
+            zer.BackColor = SystemColors.GradientActiveCaption;
+            zer.Location = new Point(12, 184);
+            zer.Name = "zer";
+            zer.Size = new Size(67, 23);
+            zer.TabIndex = 19;
+            zer.Text = "0";
+            zer.UseVisualStyleBackColor = false;
+            zer.Click += zer_Click;
+            // 
+            // dec
+            // 
+            dec.BackColor = SystemColors.GradientActiveCaption;
+            dec.Location = new Point(85, 184);
+            dec.Name = "dec";
+            dec.Size = new Size(35, 23);
+            dec.TabIndex = 20;
+            dec.Text = ".";
+            dec.UseVisualStyleBackColor = false;
+            dec.Click += dec_Click;
+            // 
+            // plus
+            // 
+            plus.BackColor = SystemColors.GradientActiveCaption;
+            plus.Location = new Point(126, 184);
+            plus.Name = "plus";
+            plus.Size = new Size(35, 23);
+            plus.TabIndex = 21;
+            plus.Text = "+";
+            plus.UseVisualStyleBackColor = false;
+            plus.Click += plus_Click;
+            // 
+            // equals
+            // 
+            equals.BackColor = SystemColors.GradientActiveCaption;
+            equals.Location = new Point(166, 155);
+            equals.Name = "equals";
+            equals.Size = new Size(54, 52);
+            equals.TabIndex = 22;
+            equals.Text = "=";
+            equals.UseVisualStyleBackColor = false;
+            equals.Click += equals_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.GradientInactiveCaption;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.RightToLeft = RightToLeft.Yes;
+            listBox1.Size = new Size(208, 49);
+            listBox1.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveCaption;
+            ClientSize = new Size(234, 216);
+            Controls.Add(listBox1);
+            Controls.Add(equals);
+            Controls.Add(plus);
+            Controls.Add(dec);
+            Controls.Add(zer);
+            Controls.Add(minus);
+            Controls.Add(three);
+            Controls.Add(two);
+            Controls.Add(one);
+            Controls.Add(recipricol);
+            Controls.Add(multiply);
+            Controls.Add(six);
+            Controls.Add(five);
+            Controls.Add(four);
+            Controls.Add(divide);
+            Controls.Add(nine);
+            Controls.Add(eight);
+            Controls.Add(seven);
+            Controls.Add(squareRoot);
+            Controls.Add(signChange);
+            Controls.Add(clear);
+            Controls.Add(clearEntry);
+            Controls.Add(back);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button back;
+        private Button clearEntry;
+        private Button clear;
+        private Button signChange;
+        private Button squareRoot;
+        private Button seven;
+        private Button eight;
+        private Button nine;
+        private Button divide;
+        private Button four;
+        private Button five;
+        private Button six;
+        private Button multiply;
+        private Button recipricol;
+        private Button one;
+        private Button two;
+        private Button three;
+        private Button minus;
+        private Button zer;
+        private Button dec;
+        private Button plus;
+        private Button equals;
+        private ListBox listBox1;
+    }
+}
---
+++ 421Lab11/Form1.cs
@@ -0,0 +1,126 @@
+namespace _421Lab11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void signChange_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void recipricol_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dec_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
